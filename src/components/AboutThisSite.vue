4b4562a4449aa5fbd57d6ab028f5123e33bd63ab
kOnkupa <tony@jd.net.nz>
2021-11-30 19:36:03 +1300

    update about page

---
+++ src/components/AboutThisSite.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="space-y-3">
+    <p class="text-2xl text-white sm:text-3xl">About this site</p>
+    <p class="text-gray-400">In case you were wondering about this site:</p>
+    <ul role="list" class="list-disc list-inside space-y-1 tracking-wide text-gray-400">
+      <li>Designed and built on a Mac using WebStorm.</li>
+      <li>Coded in Vue.js, TailwindCSS, Vite.</li>
+      <li>Hosted on Netlify.</li>
+      <li>Using beautiful Comfortaa font.</li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'AboutThisSite',
+})
+</script>
--- src/views/AboutMe.vue
+++ src/views/AboutMe.vue
@@ -1,11 +1,32 @@
 <template>
   <div>
-    <p>asf</p>
+    <div class="about-intro">
+      <p>
+        I'm The Ton Le or Tony, a New Zealand based web developer with over 2 years of experience. I specialise in front-end and back-end development with a
+        focus on usability and simplicity.
+      </p>
+      <p>
+        I've been working at Jayden Digital Ltd for almost 2 years as a Web Developer Lead. Working at JDL is a great pleasure to me because I could work to
+        work with Vue.js, React.js, Node.js, etc. and I'm very excited to continue learning and improving my skills.
+      </p>
+    </div>
+    <div class="divider" />
+    <about-this-site />
   </div>
 </template>
 <script lang="ts">
 import { defineComponent } from 'vue'
+
+import AboutThisSite from '../components/AboutThisSite.vue'
+export default {
+  components: { AboutThisSite }
+}
 defineComponent({
   name: 'AboutMe',
 })
 </script>
+<style scoped lang="css">
+.about-intro {
+  @apply text-gray-400 tracking-wide space-y-3
+}
+</style>
