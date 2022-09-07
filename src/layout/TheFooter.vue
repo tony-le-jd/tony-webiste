79dcf7e1a8b360cbfc539f462934f6182d40ed2b
Tony Le <tony@jd.net.nz>
2022-09-07 12:26:35 +1200

    add google tag and fix footer styles

--- src/layout/TheFooter.vue
+++ src/layout/TheFooter.vue
@@ -1,8 +1,12 @@
 <template>
-<footer>
-  <div class="w-full text-center py-4 text-gray-500">
-    <p>COPYRIGHT © The Ton Le</p>
-  </div>
-</footer>
+  <footer>
+    <div class="footer">
+      <p>COPYRIGHT © The Ton Le</p>
+    </div>
+  </footer>
 </template>
-
+<style scoped lang="css">
+.footer {
+  @apply w-full text-center py-4 text-gray-500;
+}
+</style>
